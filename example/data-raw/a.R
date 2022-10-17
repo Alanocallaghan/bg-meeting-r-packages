4459057fdbde2b7b1230e2a5240d2256a57111d8
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2022-10-17 13:34:13 +0100

    Tweak slides

--- example/data-raw/a.R
+++
@@ -1,4 +0,0 @@
-## code to prepare `a` dataset goes here
-
-a <- 2
-usethis::use_data(a, overwrite = TRUE)
--- example/data-raw/b.R
+++
@@ -1,4 +0,0 @@
-## code to prepare `b` dataset goes here
-
-b <- 2
-usethis::use_data(b, overwrite = TRUE)
